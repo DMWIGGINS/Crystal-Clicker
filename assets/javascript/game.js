6fa6555a3b9e4984558ad5ed723dc759be64b52d
Doreen Wiggins <Wiggy77@aol.com>
2017-10-09 12:41:02 -0400

    game update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,90 @@
-var targetscore = 0;
-var playerscore = 0;
-var wins = 0;
-var losses = 0;
-var starcrystal = 0;
-var firecrystal = 0;
-var purplecrystal = 0;
-var bluecrystal = 0;
-var turquoisecrystal = 0;
-
-
-
 $(document).ready(function () {
+
+    var targetscore = 0;
+    var playerscore = 0;
+    var wins = 0;
+    var losses = 0;
+    var whitecrystal = 0;
+    var firecrystal = 0;
+    var bluecrystal = 0;
+    var purplecrystal = 0;
+
+
+
+
     function newGame() {
         var playerscore = 0;
+
         var targetscore = Math.round((Math.random() * 101) + 19);
         console.log(targetscore);
 
-        var starcrystal = Math.round((Math.random() * 11) + 1);
-        console.log(starcrystal);
+        var whitecrystal = Math.round((Math.random() * 11) + 1);
+        console.log(whitecrystal);
 
         var firecrystal = Math.round((Math.random() * 11) + 1);
         console.log(firecrystal);
 
-        var purplecrystal = Math.round((Math.random() * 11) + 1);
-        console.log(purplecrystal);
-
         var bluecrystal = Math.round((Math.random() * 11) + 1);
         console.log(bluecrystal);
 
-        var turquoisecrystal = Math.round((Math.random() * 11) + 1);
-        console.log(turquoisecrystal);
-
+        var purplecrystal = Math.round((Math.random() * 11) + 1);
+        console.log(purplecrystal);
     }
 
-    $("starcrystal").click(function () {
-        playerscore = playerscore + starcrystal;
+    $("whitecrystal").click(function () {
+        playerscore = playerscore + whitecrystal;
         $("playerscore").append(playerscore);
-
+        if (playerscore === targetscore) {
+            wins++;
+            $("wins").append(wins);
+            newGame();
+        } else if (playerscore > targetscore) {
+            losses++;
+            ("losses").append(losses);
+            newGame();
+        }
     });
 
+    $("firecrystal").click(function () {
+        playerscore = playerscore + firecrystal;
+        $("playerscore").append(playerscore);
+        if (playerscore === targetscore) {
+            wins++;
+            $("wins").append(wins);
+            newGame();
+        } else if (playerscore > targetscore) {
+            losses++;
+            ("losses").append(losses);
+            newGame();
+        }
+    });
+
+    $("bluecrystal").click(function () {
+        playerscore = playerscore + bluecrystal;
+        $("playerscore").append(playerscore);
+        if (playerscore === targetscore) {
+            wins++;
+            $("wins").append(wins);
+            newGame();
+        } else if (playerscore > targetscore) {
+            losses++;
+            ("losses").append(losses);
+            newGame();
+        }
+    });
+
+    $("purplecrystal").click(function () {
+        playerscore = playerscore + purplecrystal;
+        $("playerscore").append(playerscore);
+        if (playerscore === targetscore) {
+            wins++;
+            $("wins").append(wins);
+            newGame();
+        } else if (playerscore > targetscore) {
+            losses++;
+            ("losses").append(losses);
+            newGame();
+        }
+    });
 
 });
