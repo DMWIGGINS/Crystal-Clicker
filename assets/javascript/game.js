1db2aec1f8f0392b87289715b7d58a10b70e9c0b
Doreen Wiggins <Wiggy77@aol.com>
2017-10-12 00:31:20 -0400

    clicking works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,123 @@
-// $(document).ready(function() {
+$(document).ready(function () {
 
-    var targetscore = 0;
-    var playerscore = 0;
-    var wins = 0;
-    var losses = 0;
-    var whitecrystal = 0;
-    var firecrystal = 0;
-    var bluecrystal = 0;
-    var purplercystal = 0;
-    
-    
-    
+// set variables for scores and crystal values
+var targetscore = 0;
+var playerscore = 0;
+var wins = 0;
+var losses = 0;
+var whitecrystal = 0;
+var firecrystal = 0;
+var bluecrystal = 0;
+var purplecrystal = 0;
 
-    function newGame() {
-        var playerscore = 0;
 
-        var targetscore = Math.round((Math.random() * 101) + 19);
-        console.log(targetscore);
 
-        var whitecrystal = Math.round((Math.random() * 11) + 1);
-        console.log(whitecrystal);
+//function to set game values back to beginning to start new game 
+function newGame() {
 
-        var firecrystal = Math.round((Math.random() * 11) + 1);
-        console.log(firecrystal);
+    // set playerscore to 0
+    playerscore = 0;
 
-        var bluecrystal = Math.round((Math.random() * 11) + 1);
-        console.log(bluecrystal);
+    // randomly generate target score between 19-120
+    targetscore = Math.round((Math.random() * 101) + 19);
+    console.log(targetscore);
 
-        var purplecrystal = Math.round((Math.random() * 11) + 1);
-        console.log(purplecrystal);
-    }
+    // randomly generate value of white crystal between 1-11
+    whitecrystal = Math.round((Math.random() * 11) + 1);
+    console.log(whitecrystal);
 
-    $("whitecrystal").click(function () {
+    // randomly generate value of fire crystal between 1-11
+    firecrystal = Math.round((Math.random() * 11) + 1);
+    console.log(firecrystal);
+
+    // randomly generate value of blue crystal between 1-11
+    bluecrystal = Math.round((Math.random() * 11) + 1);
+    console.log(bluecrystal);
+
+    // randomly generate value of purple crystal between 1-11
+    purplecrystal = Math.round((Math.random() * 11) + 1);
+    console.log(purplecrystal);
+}
+
+    // when white crystal is clicked add value to playerscore and update on gameboard
+    // compare playerscore to targetscore to see if game is over and update wins or losses as needed
+    // generate new values to start new game
+
+    $("#whitecrystal").click(function () {
+        alert("clicked white");
         playerscore = playerscore + whitecrystal;
-        console.log(whitecrystal);
-        $("playerscore").append(playerscore);
+        console.log(playerscore);
+        $("#playerscore").append(playerscore);
         if (playerscore === targetscore) {
             wins++;
-            $("wins").append(wins);
+            $("#wins").append(wins);
             newGame();
         } else if (playerscore > targetscore) {
             losses++;
-            ("losses").append(losses);
+            ("#losses").append(losses);
             newGame();
         }
     });
 
-    $("firecrystal").click(function () {
-            playerscore = playerscore + firecrystal;
-        console.log(firecrystal);
-        $("playerscore").append(playerscore);
+    // when fire crystal is clicked add value to playerscore and update on gameboard
+    // compare playerscore to targetscore to see if game is over and update wins or losses as needed
+    // generate new values to start new game
+
+    $("#firecrystal").click(function () {
+        alert("clicked fire");
+        playerscore = playerscore + firecrystal;
+        console.log(playerscore);
+        $("#playerscore").append(playerscore);
         if (playerscore === targetscore) {
             wins++;
-            $("wins").append(wins);
+            $("#wins").append(wins);
             newGame();
         } else if (playerscore > targetscore) {
             losses++;
-            ("losses").append(losses);
+            ("#losses").append(losses);
             newGame();
         }
     });
 
-    $("bluecrystal").click(function () {
+    // when blue crystal is clicked add value to playerscore and update on gameboard
+    // compare playerscore to targetscore to see if game is over and update wins or losses as needed
+    // generate new values to start new game
+
+    $("#bluecrystal").click(function () {
+        alert("clicked blue");
         playerscore = playerscore + bluecrystal;
-        console.log(bluecrystal);
-        $("playerscore").append(playerscore);
+        console.log(playerscore);
+        $("#playerscore").append(playerscore);
         if (playerscore === targetscore) {
             wins++;
-            $("wins").append(wins);
+            $("#wins").append(wins);
             newGame();
         } else if (playerscore > targetscore) {
             losses++;
-            ("losses").append(losses);
+            ("#losses").append(losses);
             newGame();
         }
     });
 
-    $("purplecrystal").click(function () {
+    // when purple crystal is clicked add value to playerscore and update on gameboard
+    // compare playerscore to targetscore to see if game is over and update wins or losses as needed
+    // generate new values to start new game
+
+    $("#purplecrystal").click(function () {
+        alert("clicked purple");
         playerscore = playerscore + purplecrystal;
-        console.log(purplecrystal);
-        $("playerscore").append(playerscore);
+        console.log(playerscore);
+        $("#playerscore").append(playerscore);
         if (playerscore === targetscore) {
             wins++;
-            $("wins").append(wins);
+            $("#wins").append(wins);
             newGame();
         } else if (playerscore > targetscore) {
             losses++;
-            ("losses").append(losses);
+            ("#losses").append(losses);
             newGame();
         }
     });
 
-// });
+    
+    });
